16b9ea697c244a2e515314d208de26db8c96a511
kirandeep kaur <kiranbhullar6042@gmail.com>
2019-08-04 22:31:14 -0400

    Add project files.

---
+++ FinalProject-StudentHelper/MainActivity.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android;
+using System;
+
+namespace FinalProject_StudentHelper
+{
+    [Obsolete]
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Fragment[] _fragmentsArray;
+  //      string name = "Welcome To my App";
+
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            // Set our view from the "main" layout resource
+            Button nextFrag = FindViewById<Button>(Resource.Id.register);
+
+            RequestWindowFeature(Android.Views.WindowFeatures.ActionBar);
+            //enable navigation mode to support tab layout
+            this.ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+
+
+            _fragmentsArray = new Fragment[]
+            {
+            new SignInFragment(""),
+            new SignUpFragmentA(),
+            };
+
+
+            AddTabToActionBar("Sign In"); //First Tab
+            AddTabToActionBar("Sign Up"); //First Tab
+
+
+        }
+
+        [Obsolete]
+        void AddTabToActionBar(string tabTitle)
+        {
+            Android.App.ActionBar.Tab tab = ActionBar.NewTab();
+            tab.SetText(tabTitle);
+
+            tab.SetIcon(Android.Resource.Drawable.IcInputAdd);
+
+            tab.TabSelected += TabOnTabSelected;
+
+            ActionBar.AddTab(tab);
+        }
+
+
+
+        void TabOnTabSelected(object sender, Android.App.ActionBar.TabEventArgs tabEventArgs)
+        {
+            Android.App.ActionBar.Tab tab = (Android.App.ActionBar.Tab)sender;
+
+            //Log.Debug(Tag, "The tab {0} has been selected.", tab.Text);
+            Fragment frag = _fragmentsArray[tab.Position];
+
+            tabEventArgs.FragmentTransaction.Replace(Resource.Id.frameLayout1, frag);
+        }
+
+
+
+    }
+}
---
+++ FinalProject-StudentHelper/SignUpFragmentA.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace FinalProject_StudentHelper
+{
+       public class SignUpFragmentA : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            View myView = inflater.Inflate(Resource.Layout.SignUpTabA, container, false);
+            Button nextFrag = myView.FindViewById<Button>(Resource.Id.register);
+            nextFrag.Click += delegate
+            {
+        //        Android.App.ActionBar.TabEventArgs tabEventArgs;
+       //         Fragment frag = new SignUpFragmentB();
+         //       _ = tabEventArgs.FragmentTransaction.Replace(Resource.Id.frameLayout1, frag);
+           //     View mV = inflater.Inflate(Resource.Layout.SignUpTabB, container, false);
+                
+            };
+           
+            return myView;
+            //return base.OnCreateView(inflater, container, savedInstanceState);
+        }
+
+    }
+}
+/*
+        void TabOnTabSelected(object sender, Android.App.ActionBar.TabEventArgs tabEventArgs)
+        {
+            Android.App.ActionBar.Tab tab = (Android.App.ActionBar.Tab)sender;
+
+            //Log.Debug(Tag, "The tab {0} has been selected.", tab.Text);
+            Fragment frag = _fragmentsArray[tab.Position];
+
+            tabEventArgs.FragmentTransaction.Replace(Resource.Id.frameLayout1, frag);
+        }
+ * */
