3661d25d09c59a7324db4bb66d4c0494c11fd1af
kirandeep kaur <kiranbhullar6042@gmail.com>
2019-08-11 17:06:30 -0400

    Profile student

--- FinalProject-StudentHelper/Resources/Resource.designer.cs
+++ FinalProject-StudentHelper/Resources/Resource.designer.cs
@@ -4274,25 +4274,28 @@ namespace FinalProject_StudentHelper
 			public const int profile = 2130968629;
 			
 			// aapt resource value: 0x7f040036
-			public const int select_dialog_item_material = 2130968630;
+			public const int ProfileStudent = 2130968630;
 			
 			// aapt resource value: 0x7f040037
-			public const int select_dialog_multichoice_material = 2130968631;
+			public const int select_dialog_item_material = 2130968631;
 			
 			// aapt resource value: 0x7f040038
-			public const int select_dialog_singlechoice_material = 2130968632;
+			public const int select_dialog_multichoice_material = 2130968632;
 			
 			// aapt resource value: 0x7f040039
-			public const int SignIn = 2130968633;
+			public const int select_dialog_singlechoice_material = 2130968633;
 			
 			// aapt resource value: 0x7f04003a
-			public const int SignUp = 2130968634;
+			public const int SignIn = 2130968634;
 			
 			// aapt resource value: 0x7f04003b
-			public const int SignUpTabA = 2130968635;
+			public const int SignUp = 2130968635;
 			
 			// aapt resource value: 0x7f04003c
-			public const int support_simple_spinner_dropdown_item = 2130968636;
+			public const int SignUpTabA = 2130968636;
+			
+			// aapt resource value: 0x7f04003d
+			public const int support_simple_spinner_dropdown_item = 2130968637;
 			
 			static Layout()
 			{
